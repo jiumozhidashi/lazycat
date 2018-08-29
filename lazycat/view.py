b16d0f6ee0a7c2caa4c0123ecd9f317635d70b41
Jacky.lei <20826250@qq.com>
2018-08-29 15:14:51 +0800

    初次完成单元格循环写入代码

--- lazycat/view.py
+++ lazycat/view.py
@@ -39,7 +39,13 @@ def UploadFile(request):
     f.close()
     wb=load_workbook('static/downloads/%s'%(obj.name))
     oldws=wb.active
-    for cell in oldws['4']:
+    station_row=4
+    station_start_col=6
+    material_start_row=7
+    materialinfo_start_col = 2
+    materialinfo_end_col=4
+    materialqty_start_col=6
+    for cell in oldws[station_row]:
         if cell.value=="单价(元)":
             break
         elif cell.value!=None:
@@ -47,22 +53,35 @@ def UploadFile(request):
         else:cell.value=s
         #print(cell.coordinate,cell.value)
     newws=wb.create_sheet(title='formatsheet')
-    data=oldws.values
+    #data=oldws.values
     '''print('stations:')
     for n in range(1,5):
      stations=next(data)[5:]
     for station in stations:
        print(station)'''
     print('stations_rows:')
-    for col in oldws.iter_cols(min_row=4,max_row=4,min_col=6):
-         for c in col:
-            print(c.value)
-    for row in oldws.iter_rows(min_row=7,min_col=2,max_col=4):
-        for r in row:
-            print(r.value)
-    for row in oldws.iter_rows(min_row=7,min_col=6):
-        for r in row:
-            print(r.value)
+
+    write_row = 2
+    write_col=1
+    read_row=material_start_row
+    read_col=station_start_col
+    for st_col in oldws.iter_cols(min_row=station_row,max_row=station_row,min_col=station_start_col):
+         for station_cell in st_col:
+             if station_cell.value == "单价(元)":
+                 break
+             else:
+                 for ma_row in oldws.iter_rows(min_row=material_start_row,min_col=materialinfo_start_col,max_col=materialinfo_end_col):
+                    for r in ma_row:
+                        newws.cell(row=write_row,column=write_col,value=r.value)
+                        write_col+=1
+                    newws.cell(row=write_row,column=write_col+1,value=oldws.cell(row=read_row,column=read_col).value)
+                    newws.cell(row=write_row,column=write_col+2,value=station_cell.value)
+                    write_row+=1
+                    read_row+=1
+             read_col+=1
+    '''for row in oldws.iter_rows(min_row=7,min_col=6):
+        for r in row:'''
+            #print(r.value)
     '''materials=[r[1:4] for r in data]
     specs=[r[2] for r in data ]
     units=[r[3] for r in data ]
